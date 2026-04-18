dd7bf6e83f8eb72c221163c75280e73feb5e8eec
Bianca <dancibianca1@gmail.com>
2026-04-18 09:20:30 +0300

    Data added

--- Hangman/Services/UserService.cs
+++ Hangman/Services/UserService.cs
@@ -4,8 +4,9 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows;
 
 namespace Hangman.Services
 {
@@ -18,6 +19,15 @@ namespace Hangman.Services
             var dataDir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data");
             Directory.CreateDirectory(dataDir); // asigură că directorul există
             _filePath = Path.Combine(dataDir, "users.json");
+
+            if (!File.Exists(_filePath))
+            {
+                var projectDirUsersFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\Data\users.json");
+                if (File.Exists(projectDirUsersFile))
+                {
+                    File.Copy(projectDirUsersFile, _filePath);
+                }
+            }
         }
 
         public List<User> LoadUsers()
@@ -27,10 +37,15 @@ namespace Hangman.Services
             try
             {
                 var json = File.ReadAllText(_filePath);
+                var options = new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                };
                 return JsonSerializer.Deserialize<List<User>>(json) ?? new List<User>();
             }
-            catch
+            catch(Exception ex)
             {
+                MessageBox.Show($"Eroare la incarcarea utilizatorilor din \n{_filePath}\n\nEroare: {ex.Message}", "JSON Error", MessageBoxButton.OK, MessageBoxImage.Error);
                 return new List<User>();
             }
         }
